3ee4a324895a8b2ffdaf034338c8ab22819108bc
Britta <polished.athena@gmail.com>
2019-03-10 15:47:55 -0500

    updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+// states guess what letter I'm thinking
+
+//set variables
+
+//set up random guess by computer
+
+//set up keys for player's guess
+
+
+//set objects?
+
+//set up key responses
+
+//set up you win alert in console.log
+
+//set up you loose alert console.log
+
+//set up counter
